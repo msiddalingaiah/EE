021145880cb0c0e4e8d1c01afc0712a08284d839
msiddalingaiah <madhu@madhu.com>
2023-07-15 14:02:26 -0400

    Refactor

--- NandlandGo/BinaryTo7Segment.v
+++ NandlandGo/BinaryTo7Segment.v
@@ -1,28 +1,23 @@
 
-module BinaryTo7Segment (input clock, input [3:0] bcd, output [6:0] digit);
-  reg [6:0] hex_decode = 7'h00;
-   
-  always @(posedge clock)
-    begin
-      case (bcd)
-        4'b0000 : hex_decode <= 7'h7E;
-        4'b0001 : hex_decode <= 7'h30;
-        4'b0010 : hex_decode <= 7'h6D;
-        4'b0011 : hex_decode <= 7'h79;
-        4'b0100 : hex_decode <= 7'h33;          
-        4'b0101 : hex_decode <= 7'h5B;
-        4'b0110 : hex_decode <= 7'h5F;
-        4'b0111 : hex_decode <= 7'h70;
-        4'b1000 : hex_decode <= 7'h7F;
-        4'b1001 : hex_decode <= 7'h7B;
-        4'b1010 : hex_decode <= 7'h77;
-        4'b1011 : hex_decode <= 7'h1F;
-        4'b1100 : hex_decode <= 7'h4E;
-        4'b1101 : hex_decode <= 7'h3D;
-        4'b1110 : hex_decode <= 7'h4F;
-        4'b1111 : hex_decode <= 7'h47;
-      endcase
-    end
- 
-  assign digit = hex_decode;
+module BinaryTo7Segment (input clock, input [3:0] bcd, output reg [6:0] segments);
+  always @(posedge clock) begin
+    case (bcd)
+      4'b0000 : segments <= 7'h7E;
+      4'b0001 : segments <= 7'h30;
+      4'b0010 : segments <= 7'h6D;
+      4'b0011 : segments <= 7'h79;
+      4'b0100 : segments <= 7'h33;          
+      4'b0101 : segments <= 7'h5B;
+      4'b0110 : segments <= 7'h5F;
+      4'b0111 : segments <= 7'h70;
+      4'b1000 : segments <= 7'h7F;
+      4'b1001 : segments <= 7'h7B;
+      4'b1010 : segments <= 7'h77;
+      4'b1011 : segments <= 7'h1F;
+      4'b1100 : segments <= 7'h4E;
+      4'b1101 : segments <= 7'h3D;
+      4'b1110 : segments <= 7'h4F;
+      4'b1111 : segments <= 7'h47;
+    endcase
+  end
 endmodule
--- NandlandGo/DebounceSwitch.v
+++ NandlandGo/DebounceSwitch.v
@@ -1,26 +1,19 @@
 
-module DebounceSwitch (input clock, input i_Switch, output o_Switch);
-  parameter c_DEBOUNCE_LIMIT = 250000;  // 10 ms at 25 MHz
-   
-  reg [17:0] r_Count = 0;
-  reg r_State = 1'b0;
-   // Assign internal register to output (debounced!)
-  assign o_Switch = r_State;
+module DebounceSwitch (input clock, input sw_input, output debounced);
+  parameter DEBOUNCE_COUNT = 25000;  // 1 ms at 25 MHz
+
+  reg [17:0] count = DEBOUNCE_COUNT;
+  reg state = 1'b0;
+  assign debounced = state;
 
   always @(posedge clock) begin
-    // Switch input is different than internal switch value, so an input is
-    // changing.  Increase the counter until it is stable for enough time.  
-    if (i_Switch !== r_State && r_Count < c_DEBOUNCE_LIMIT)
-      r_Count <= r_Count + 1;
- 
-    // End of counter reached, switch is stable, register it, reset counter
-    else if (r_Count == c_DEBOUNCE_LIMIT) begin
-      r_State <= i_Switch;
-      r_Count <= 0;
-    end 
- 
-    // Switches are the same state, reset the counter
-    else
-      r_Count <= 0;
+    if (sw_input != state) begin
+      if (count != 0) begin
+        count <= count - 1;
+      end else begin
+        count <= DEBOUNCE_COUNT;
+        state <= sw_input;
+      end
+    end
   end 
 endmodule
