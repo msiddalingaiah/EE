663d0d77b1aebd9a760022866043867474ddac60
msiddalingaiah <madhu@madhu.com>
2023-08-18 21:03:28 -0400

    More unit test

--- RiscV/main.c
+++ RiscV/main.c
@@ -3,11 +3,13 @@ void write_uart(int c);
 void write_uart_str(char *p);
 void halt();
 void test_compare();
+void test_alu(int a, int b);
 
 void main() {
     unsigned char *p = (unsigned char *) "Hello World!\n";
     write_uart_str(p);
     test_compare();
+    test_alu(4, 6);
     halt();
     for(;;);
 }
@@ -25,6 +27,30 @@ void write_uart_str(char *p) {
 void halt() {
     unsigned int *p = (unsigned int *) 0xf0000020;
     *p = 0xc0de;
+}
+
+void test_alu(int a, int b) {
+    if (a+3 == 7) {
+        write_uart('.');
+    } else {
+        write_uart('F');
+    }
+    if (a-7 == -3) {
+        write_uart('.');
+    } else {
+        write_uart('F');
+    }
+    if (a+b == 10) {
+        write_uart('.');
+    } else {
+        write_uart('F');
+    }
+    if (a-b == -2) {
+        write_uart('.');
+    } else {
+        write_uart('F');
+    }
+    write_uart('\n');
 }
 
 void test_compare() {
