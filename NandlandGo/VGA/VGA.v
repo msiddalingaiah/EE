20f9b943b1c0255a92149076dcc255da723cf2f2
msiddalingaiah <madhu@madhu.com>
2024-11-25 12:12:00 -0500

    +VGA working Pong

--- NandlandGo/VGA/VGA.v
+++ NandlandGo/VGA/VGA.v
@@ -52,20 +52,11 @@ module VGA (
     reg [9:0] column, row;
     reg [2:0] red, green, blue;
     reg [8:0] color;
-    reg draw;
 
-    reg [9:0] ball_x, ball_y, dx, dy;
+    reg [9:0] ball_x, ball_y, ball_dx, ball_dy;
+    reg [9:0] paddle_left_y, paddle_right_y;
 
-    initial begin
-        row = 0;
-        column = 0;
-        color = 9'b111111000;
-        ball_x = 43;
-        ball_y = 71;
-        dx = 1;
-        dy = 1;
-        draw = 0;
-    end
+    reg [1:0] state;
 
     localparam H_ACTIVE = 640;
     localparam H_FPORCH = 16;
@@ -76,7 +67,26 @@ module VGA (
     localparam V_FPORCH = 10;
     localparam V_PULSE = 2;
     localparam V_MAX = 525;
-    
+    localparam PADDLE_HEIGHT_2 = 30;
+    localparam PADDLE_WIDTH = 10;
+    localparam BALL_SIZE_2 = 3;
+
+    localparam IDLE = 0;
+    localparam PLAY = 1;
+
+    initial begin
+        row = 0;
+        column = 0;
+        color = 0;
+        ball_x = 43;
+        ball_y = 71;
+        ball_dx = 1;
+        ball_dy = 1;
+        paddle_left_y = V_ACTIVE >> 1;
+        paddle_right_y = V_ACTIVE >> 1;
+        state = IDLE;
+    end
+
     // See https://vanhunteradams.com/DE1/VGA_Driver/Driver.html
     assign o_VGA_HSync = (column < H_ACTIVE+H_FPORCH || column >= H_ACTIVE+H_FPORCH+H_PULSE) ? 1 : 0;
     assign o_VGA_VSync = (row < V_ACTIVE+V_FPORCH || row >= V_ACTIVE+V_FPORCH+V_PULSE) ? 1 : 0;
@@ -84,12 +94,22 @@ module VGA (
     assign { o_VGA_Grn_2, o_VGA_Grn_1, o_VGA_Grn_0 } = (column < H_ACTIVE && row < V_ACTIVE) ? green : 0;
     assign { o_VGA_Blu_2, o_VGA_Blu_1, o_VGA_Blu_0  } = (column < H_ACTIVE && row < V_ACTIVE) ? blue : 0;
 
-    assign { red, green, blue } = draw ? color : 0;
+    assign { red, green, blue } = color;
 
     always @(*) begin
         draw = 0;
-        if ((ball_y - row) < 5 && (ball_x - column) < 5) begin
-            draw = 1;
+        color = 0;
+        if ((ball_y - row < BALL_SIZE_2 || row - ball_y < BALL_SIZE_2)
+                && (ball_x - column < BALL_SIZE_2 || column - ball_x < BALL_SIZE_2)) begin
+            color = 9'b000111000;
+        end
+        if ((paddle_left_y - row < PADDLE_HEIGHT_2 || row - paddle_left_y < PADDLE_HEIGHT_2)
+                && column < PADDLE_WIDTH) begin
+            color = 9'b111111000;
+        end
+        if ((paddle_right_y - row < PADDLE_HEIGHT_2 || row - paddle_right_y < PADDLE_HEIGHT_2)
+                && column > H_ACTIVE-PADDLE_WIDTH) begin
+            color = 9'b000111111;
         end
     end
 
@@ -100,28 +120,46 @@ module VGA (
         end
 
         if (led_count[16:0] == 0) begin
-            ball_x <= ball_x + dx;
-            ball_y <= ball_y + dy;
+            if (state == IDLE) begin
+                ball_x <= H_ACTIVE >> 1;
+                ball_y <= V_ACTIVE >> 1;
+                if (i_Switch_1) state <= PLAY;
+            end
+            if (state == PLAY) begin
+                ball_x <= ball_x + ball_dx;
+                ball_y <= ball_y + ball_dy;
+            end
             if (ball_x == H_ACTIVE) begin
-                ball_x <= H_ACTIVE-1;
-                dx <= ~dx + 1;
-                color <= 9'b111000000;
+                if (ball_y - paddle_right_y < PADDLE_HEIGHT_2 || paddle_right_y - ball_y < PADDLE_HEIGHT_2) begin
+                    ball_x <= H_ACTIVE-1;
+                    ball_dx <= ~ball_dx + 1;
+                end else begin
+                    // Left player wins..
+                    state <= IDLE;
+                end
             end
             if (ball_x == 0) begin
-                ball_x <= 1;
-                dx <= ~dx + 1;
-                color <= 9'b000111000;
+                if (ball_y - paddle_left_y < PADDLE_HEIGHT_2 || paddle_left_y - ball_y < PADDLE_HEIGHT_2) begin
+                    ball_x <= 1;
+                    ball_dx <= ~ball_dx + 1;
+                end else begin
+                    // Right player wins..
+                    state <= IDLE;
+                end
             end
             if (ball_y == V_ACTIVE) begin
                 ball_y <= V_ACTIVE-1;
-                dy <= ~dy + 1;
-                color <= 9'b000000111;
+                ball_dy <= ~ball_dy + 1;
             end
             if (ball_y == 0) begin
                 ball_y <= 1;
-                dy <= ~dy + 1;
-                color <= 9'b000111111;
+                ball_dy <= ~ball_dy + 1;
             end
+
+            if (i_Switch_1 && paddle_left_y > PADDLE_HEIGHT_2) paddle_left_y <= paddle_left_y - 1;
+            if (i_Switch_2 && paddle_left_y < V_ACTIVE-PADDLE_HEIGHT_2) paddle_left_y <= paddle_left_y + 1;
+            if (i_Switch_3 && paddle_right_y > PADDLE_HEIGHT_2) paddle_right_y <= paddle_right_y - 1;
+            if (i_Switch_4 && paddle_right_y < V_ACTIVE-PADDLE_HEIGHT_2) paddle_right_y <= paddle_right_y + 1;
         end
 
         column <= column + 1;
